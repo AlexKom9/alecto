53d676498412e6d1051383cb4480b6d9196621c3
Alexandr Komar <909komar@gmail.com>
2018-02-28 11:23:50 +0200

    fix

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+;
+$(document).ready(function(){
+    $('#slides').slick({
+        prevArrow: $('.slider__arrow--prev'),
+        nextArrow: $('.slider__arrow--next'),
+        // dots: true,
+        // dotsClass: 'slider__dots',
+        // appendDots: $('.slider__dots-bar')
+        
+    });
+
+});
